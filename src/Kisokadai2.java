6247accfdaa867711a30a22a767f7657883ea137
Taiga Kasamatsu <stoic-music-life@i.softbank.jp>
2015-11-24 13:27:30 +0900

    修正しました。

--- src/Kisokadai2.java
+++ src/Kisokadai2.java
@@ -1,51 +1,76 @@
-import java.util.Random;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Kisokadai2 {
 
-	public static void main(String[] args) {
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		
+		System.out.println("数当てゲームを始めます！\n１～１００までの数字をコンピューターが\n"
+				+ "ランダムに決めます！当ててみて下さい！");
+		
+		int i =(int) (Math.random() * 100) + 1;
 
-		Scanner s = new Scanner(System.in);
-		Random rnd = new Random();
-
-		int num = rnd.nextInt(101);
-
-		System.out.println("数当てゲームの始まりです。");
-		System.out.println("１～１００まで数字を当てて下さい！");
-
-		int a;
-		a = s.nextInt();
-
-		do {
-			System.out.println("");
-
-			a = s.nextInt();
-			if (a == 0 || 101 <= a)
-				System.out.println("１～１００までの値を入力して下さい。");
-
-			if (num > a)
-				if ((num - a) <= 100 && (num - a) >= 50)
-					System.out.println("全然上！");
-				else if ((num - a) <= 49 && (num - a) >= 20)
-					System.out.println("もう少し上");
-				else if ((num - a) <= 19 && (num - a) >= 5)
-					System.out.println("近い！もちっと上");
-				else if ((num - a) <= 4 && (num - a) >= 1)
+		String str = null;
+		boolean fl = true;
+		
+		
+		while(fl)
+			
+			try{
+				BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+				str = br .readLine();
+		 if(isNumber(str)){
+			 int a =Integer.parseInt(str);
+			 
+			 if((i - a <= 100 && (i - a) >= 50))
+			 System.out.println("はるか上です！");
+			 else if ((i - a) <= 49 && (i - a) >= 20)
+					System.out.println("もっと上です！");
+				else if ((i - a) <= 19 && (i - a) >= 5)
+					System.out.println("近い！もちっと上です！");
+				else if ((i - a) <= 4 && (i - a) >= 1)
 					System.out.println("惜しい、わずかに上");
+			 
+			 if (i < a)
+					if ((a - i) <= 100 && (a - i) >= 50)
+						System.out.println("はるか下です！");
+					else if ((a - i) <= 49 && (a - i) >= 20)
+						System.out.println("もっと下です！");
+					else if ((a - i) <= 19 && (a - i) >= 5)
+						System.out.println("近い！もちっと下です！");
+					else if ((a - i) <= 4 && (a -i ) >= 1)
+						System.out.println("惜しい！わずかに下です！");
+			 if(a==i)
+				 System.out.print("☆大正解☆");
+			 		else {
+						// 1～100以外の数値を入力した場合
+						System.out.println("１～１００までの値を入力して下さい！");
+							continue;
 
-			if (num < a)
-				if ((a - num) <= 100 && (a - num) >= 50)
-					System.out.println("全然下！");
-				else if ((a - num) <= 49 && (a - num) >= 20)
-					System.out.println("もう少し下");
-				else if ((a - num) <= 19 && (a - num) >= 5)
-					System.out.println("近い！もちっと下");
-				else if ((a - num) <= 4 && (a - num) >= 1)
-					System.out.println("惜しい！わずかに下");
-
-		} while (num != a);
-
-		System.out.println("大～～～当たり！！！");
+					}
+				} else {
+					// 数値以外を入力した場合
+					System.out.println("１～１００までの値を入力して下さい。");
+					continue;
+					
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+				System.out.print("☆大正解☆");
+			}
 	}
 
-}
+			// 数値型チェック
+			public static boolean isNumber(String val) {
+				try {
+					Integer.parseInt(val);
+					return true;
+				} catch (NumberFormatException nfex) {
+					return false;
+				}
+			}
+		}
